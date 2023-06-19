cbbe66e630cc3c96d8e9a8f6054b5a2911e6c748
subbum <subbu@gmail.com>
2023-06-19 14:44:43 +0100

    variables updated with quotes

--- vars.tf
+++ vars.tf
@@ -1,13 +1,13 @@
-variable env{}
-variable subnet_ids {}
-variable tags {}
-variable engine {}
-variable engine_version {}
-variable database_name {
+variable "env" {}
+variable "subnet_ids" {}
+variable "tags" {}
+variable "engine" {}
+variable "engine_version" {}
+variable "database_name" {
     default = "dummy"
 }
-variable backup_retention_period {}
-variable preferred_backup_window {}
-variable no_of_instances {}
-variable instance_class {}
+variable "backup_retention_period" {}
+variable "preferred_backup_window" {}
+variable "no_of_instances" {}
+variable "instance_class" {}
 
